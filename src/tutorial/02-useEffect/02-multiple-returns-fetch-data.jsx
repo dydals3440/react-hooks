4de163593bafbb402e0fea60d5b7d502e559678d
김용민 <dydals3440@gmail.com>
2023-06-03 20:28:27 +0900

    feat:ReactHooks Rules

--- src/tutorial/02-useEffect/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/02-useEffect/02-multiple-returns-fetch-data.jsx
@@ -5,21 +5,28 @@ export const MultipleReturnFechDataReal = () => {
   const [isLaoding, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
   const [user, setUser] = useState(null);
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const response = await fetch(url);
-        const user = await response.json();
-        console.log(user);
-        // user의 정보를 전달한 후에, catch문 끝나고 loading을 끝나는 처리를 해줘야, 우리가 로딩화면이 아닌, FetchData를 받은 화면을 볼 수 있음.ㄴ
-        setUser(user);
-      } catch (err) {
-        // 에러 발생시 true로 변경
+  const fetchUser = async () => {
+    try {
+      const response = await fetch(url);
+      // fetch를 하면 4xx, 5xx 번호대 에러를 따로 분기해서 또 처리해주어야함 try부분에서
+      if (!response.ok) {
         setIsError(true);
-        console.log(err);
-      } // catch문 끝난 후, 로딩을 반대로 해주어야함
-      setIsLoading(false);
-    };
+        setIsLoading(false);
+      }
+      const user = await response.json();
+      console.log(response);
+      // user의 정보를 전달한 후에, catch문 끝나고 loading을 끝나는 처리를 해줘야, 우리가 로딩화면이 아닌, FetchData를 받은 화면을 볼 수 있음.ㄴ
+      setUser(user);
+    } catch (err) {
+      // 에러 발생시 true로 변경
+      setIsError(true);
+      console.log(err);
+    } // catch문 끝난 후, 로딩을 반대로 해주어야함
+    setIsLoading(false);
+  };
+
+  // 여기서 디펜던시에, fetchData()를 넣으면, 리렌더링되면서, 다시 fetchUser함수가 실행되고
+  useEffect(() => {
     fetchUser();
   }, []);
   if (isLaoding) {
@@ -28,16 +35,20 @@ export const MultipleReturnFechDataReal = () => {
   if (isError) {
     return <h2>에러가 발생했습니다.</h2>;
   }
+  // const [user, setUser] = null 아래에 두면 에러 발생 null이기 떄문에 구조 분해 할당을 받지 못함, multiple return이 있는 경우, 구조분해할당 할떄, 조건문 다음에 구조분해할당을 해야함, 안그러면 null상태로 초기화했기 때문에 에러가 발생됨
+  const { avatar_url, name, company, bio } = user;
   return (
     <div>
       <img
         style={{ width: '150px', borderRadius: '25px' }}
-        src={user.avatar_url}
-        alt={user.name}
+        src={avatar_url}
+        alt={name}
       />
-      <h2>{user.name}</h2>
-      <h4>{user.company}에서 근무중...</h4>
-      <p>{user.bio}</p>
+      <h2>{name}</h2>
+      <h4>{company}에서 근무중...</h4>
+      <p>{bio}</p>
     </div>
   );
 };
+
+// fetch는 4xx대, 5xx대 에러를 에러로 간주하지 않음 이것을 성공인 경우로 봄
