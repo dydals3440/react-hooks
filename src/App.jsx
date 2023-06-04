3b9a265c5dc1dc3ee5dc9b539cefc1d331f7d826
김용민 <dydals3440@gmail.com>
2023-06-04 19:15:16 +0900

    feat: checkbox value control

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css';
 import { MultipleInputs } from './tutorial/05-controlled-inputs.jsx/02-multiple-inputs';
+import { CheckBox } from './tutorial/05-controlled-inputs.jsx/03-checkBox';
 
 function App() {
   return (
     <>
-      <MultipleInputs />
+      <CheckBox />
     </>
   );
 }
--- src/tutorial/05-controlled-inputs.jsx/03-checkBox.jsx
+++ src/tutorial/05-controlled-inputs.jsx/03-checkBox.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import { useState } from 'react';
 
 export const CheckBox = () => {
+  const [shipping, setShipping] = useState(false);
+  const handleShipping = (e) => {
+    console.log(e.target.checked);
+    setShipping(e.target.checked);
+  };
   return (
     <div>
       <form className='form'>
         <h4>Other Inputs</h4>
         <div className='form-row' style={{ textAlign: 'left' }}>
           <label htmlFor='shipping'>무료 배송</label>
-          <input type='checkbox' />
+          <input
+            type='checkbox'
+            name='shipping'
+            id='shipping'
+            checked={shipping}
+            onChange={handleShipping}
+          />
         </div>
         <div className='form-row' style={{ textAlign: 'left' }}>
           <label htmlFor='framework'>Framework</label>
