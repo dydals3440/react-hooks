a018e3ee79cf923986666cc3a926cf6835e29aa0
김용민 <dydals3440@gmail.com>
2023-06-08 21:06:51 +0900

    feat: useRef focusing 리렌더링이 없다

--- src/tutorial/06-useRef2/UseRefBasics.jsx
+++ src/tutorial/06-useRef2/UseRefBasics.jsx
@@ -2,17 +2,29 @@ import { useEffect, useRef, useState } from 'react';
 
 export const UseRefBasics = () => {
   const [value, setValue] = useState(0);
-  const refContainer = useRef(null);
+  const refContainer = useRef(null); // {current: null}
+  const isMounted = useRef(false); // {current: false}
   // console.log(refContainer); // 애는 null을 가리킬꺼임 ref를 지정해줘도 처음에는 null이기 떄문에
 
   useEffect(() => {
-    // console.log(refContainer); // useEffect 호출로 인해, ref가 연결되있는 input을 연결 DOM NODE를 얻음
+    // 페이지가 Refresh되면, 인풋창에 focus
+    refContainer.current.focus();
   });
+
+  useEffect(() => {
+    // console.log(refContainer); // useEffect 호출로 인해, ref가 연결되있는 input을 연결 DOM NODE를 얻음
+    if (!isMounted.current) {
+      console.log(isMounted);
+      isMounted.current = true;
+      return;
+    }
+    console.log('re-render');
+  }, [value]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     // {current: input#name.form-input 이므로}
-    // useRef를 통해 uncontrolled input을 갖고올 수 있음.
+    // useRef를 통해 uncontrolled input값을 받아올 수 있음.
     const name = refContainer.current.value;
     console.log(name);
   };
@@ -30,9 +42,9 @@ export const UseRefBasics = () => {
             ref={refContainer}
           />
         </div>
-        <div className='btn btn-block' type='submit'>
+        <button className='btn btn-block' type='submit'>
           제출하기
-        </div>
+        </button>
       </form>
       <h1>value : {value}</h1>
       <button onClick={() => setValue(value + 1)}>증가하기</button>
